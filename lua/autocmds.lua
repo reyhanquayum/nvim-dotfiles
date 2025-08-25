545123f65ac01e7c49e255b0a68dc8eaad28a3eb
reyhanquayum <reyhan.quayum@gmail.com>
2025-08-25 19:16:21 -0400

    blink-cmp keymaps and glow config

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,54 @@
 require "nvchad.autocmds"
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Typst writing optimizations
+autocmd("FileType", {
+  pattern = "typst",
+  callback = function()
+    -- Essential for writing
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.breakindent = true
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = "en_us"
+    
+    -- Writing-friendly editing
+    vim.opt_local.textwidth = 0        -- No hard line breaks
+    vim.opt_local.wrapmargin = 0       -- No automatic wrapping
+    vim.opt_local.formatoptions:remove("t") -- Don't auto-wrap text
+    
+    -- Better for prose
+    vim.opt_local.conceallevel = 2     -- Hide markup syntax when not on line
+    vim.opt_local.concealcursor = "nc" -- Hide in normal/command mode
+    
+    -- Indentation for structured content
+    vim.opt_local.expandtab = true
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})
+
+-- Markdown for comparison/other writing
+autocmd("FileType", {
+  pattern = { "markdown", "text", "tex" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.breakindent = true
+    vim.opt_local.spell = true
+    vim.opt_local.textwidth = vim.fn.winwidth(0) - 5
+    vim.opt_local.formatoptions:append("t") -- auto wrap text
+    vim.opt_local.conceallevel = 2
+  end,
+})
+
+-- Auto-save for writing files (super helpful for essays!)
+autocmd({ "TextChanged", "TextChangedI" }, {
+  pattern = { "*.typ", "*.md", "*.tex", "*.txt" },
+  callback = function()
+    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" then
+      vim.cmd("silent! write")
+    end
+  end,
+})
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,7 +4,6 @@ return {
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
-
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
@@ -12,17 +11,45 @@ return {
       require "configs.lspconfig"
     end,
   },
-
+  {
+    "ellisonleao/glow.nvim",
+    ft = "markdown",
+    config = function()
+      require("glow").setup({
+        border = "shadow",
+        pager = false,
+        width = 80,
+        height = 100,
+        width_ratio = 0.7,
+        height_ratio = 0.7,
+        })
+          end,
+        },
   -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  { import = "nvchad.blink.lazyspec" },
+  
+  -- Override blink-cmp keymaps
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "default",
+        ["<CR>"] = {}, -- Disable Enter for completion
+        ["<C-y>"] = { "accept" }, -- Accept with Ctrl+Y
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+        ["<C-e>"] = { "cancel" },
+      }
+    }
+  },
+  
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc",
+        "html", "css"
+      },
+    },
+  },
 }
